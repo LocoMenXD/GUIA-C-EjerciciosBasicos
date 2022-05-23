50d41d627f59664e450771f72ee800cda41c75c5
LocoMenXD <105610399+LocoMenXD@users.noreply.github.com>
2022-05-23 14:48:51 -0400

    Add files via upload

---
+++ EjericiciosPracticarFactorial/EjericiciosPracticarFactorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjericiciosPracticarFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("El Factorial De Un Numero");
+            int n = 0;
+            int c = 0;
+            int factorial = 1;
+            Console.WriteLine("Ingrese Un  Numero");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <=n; i++)
+
+            {
+
+                c = c + 1;
+                factorial = factorial * c;
+            }
+
+            Console.WriteLine("El Factorial De " + n + " es: "+ factorial);
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ EjericiciosPracticarFactorial/EjericioGuiaSHARPrecorrer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjericioGuiaSHARPrecorrer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 100) 
+            {
+                Console.WriteLine(i);
+                i++; // i + i = 1;
+
+            }
+           Console.ReadLine();
+        }
+    }
+}
